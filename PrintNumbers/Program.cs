de5d285aaa82a2914688e9aca27b48c70706ab30
MayankMehta <mehta.v.mayank@gmail.com>
2016-11-17 23:49:01 -0500

    Commit

---
+++ PrintNumbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace PrintNumbers
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var list = new List<int> { 0, 2, 3, 6 };
+            printNumbers(list);
+        }
+
+        private static void printNumbers(List<int> i)
+        {
+            foreach (int y in i)
+            {
+                if (y % 3 == 0 && y % 5 == 0)
+                {
+                    Debug.WriteLine("lutron");
+                }
+                else if (y % 3 == 0)
+                {
+                    Debug.WriteLine("lu");
+                }
+                else if (y % 5 == 0)
+                {
+                    Debug.WriteLine("tron");
+                }
+            }
+        }
+    }
+}
---
+++ PrintNumbersRecursive/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace PrintNumbersRecursive
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int i = 1;
+            printNumbers(i);
+        }
+
+        private static void printNumbers(int y)
+        {
+            if (y == 101)
+            {
+                Debug.WriteLine("END!" + y);
+                return;
+            }
+
+            if (y % 3 == 0 && y % 5 == 0)
+            {
+                Debug.WriteLine("lutron" + y);
+            }
+            else if (y % 3 == 0)
+            {
+                Debug.WriteLine("lu" + y);
+            }
+            else if (y % 5 == 0)
+            {
+                Debug.WriteLine("tron" + y);
+            }
+            y++;
+            printNumbers(y);
+        }
+    }
+}
